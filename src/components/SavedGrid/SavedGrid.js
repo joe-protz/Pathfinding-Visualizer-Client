dc43a6cf0c5253d52bfa4edc0687a358a5a8344a
joe10994 <joe10994@gmail.com>
2020-03-09 17:43:40 -0400

    Update SavedGrid Component
    - Previously,the component did not handle name updates
    - Added messages for success/failure of update
    Close #9

--- src/components/SavedGrid/SavedGrid.js
+++ src/components/SavedGrid/SavedGrid.js
@@ -6,116 +6,147 @@ import Cell from '../Shared/Cell'
 import axios from 'axios'
 // -----------API URL
 import apiUrl from '../../apiConfig'
+// -----------GridForm
+import GridForm from '../Shared/GridForm'
 
 export default class SavedGrid extends Component {
   constructor () {
     super()
     this.state = {
       found: false,
-      owned: false
+      owned: false,
+      updated: false
     }
   }
-  cells
-  cols
-  cellTest
-  rows
-  changed = false
-  scale = 20
-  // TODO: Make breakpoints to have this be responsive on mobile:
-  // scale down and also make the canvas resize if the window is under a certain width
+cells
+cols
+cellTest
+rows
+scale = 20
+// TODO: Make breakpoints to have this be responsive on mobile:
+// scale down and also make the canvas resize if the window is under a certain width
 
-  componentDidMount () {
-    // get the grid by id
-    axios({
-      url: apiUrl + '/grids/' + this.props.match.params.id,
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${this.props.user.token}`
-      }
-    })
-      .then(res => {
-        console.log(res.data.grid)
-        this.cells = res.data.grid.walls
-        this.setState({ found: true,
-          owned: res.data.grid.editable
-        })
+componentDidMount () {
+  // get the grid by id
+  axios({
+    url: apiUrl + '/grids/' + this.props.match.params.id,
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${this.props.user.token}`
+    }
+  })
+    .then(res => {
+      this.cells = res.data.grid.walls
+      this.setState({
+        found: true,
+        owned: res.data.grid.editable,
+        grid: { name: res.data.grid.name },
+        gridId: res.data.grid._id
       })
-      .catch(console.error)
+    })
+    .catch(console.error)
+}
+
+handleChange = event => {
+  const { name, value } = event.target
+  const updatedField = {
+    [name]: value
   }
-  // set up the canvas
-  setup = (p5, canvasParentRef) => {
-    // create canvas
+  const editedGrid = Object.assign(
+    this.state.grid,
+    updatedField
+  )
+  this.setState({ grid: editedGrid })
+}
+
+// set up the canvas
+setup = (p5, canvasParentRef) => {
+  // create canvas
+  // canvas is 600x500px
+  p5.createCanvas(600, 500).parent(canvasParentRef) // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
+  this.cols = Math.ceil(p5.width / this.scale)
+  this.rows = Math.ceil(p5.height / this.scale)
+}
+
+draw = p5 => {
+  // this needs to be done here because component did mount happens
+  // after setup, so in order to use the response data I created a
+  // boolean check since draw is a continual loop
+  if (this.state.found && !this.updated) {
     const myP5 = p5
-    // canvas is 600x500px
-    p5.createCanvas(600, 500).parent(
-      canvasParentRef
-    ) // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
-    this.cols = Math.ceil(p5.width / this.scale)
-    this.rows = Math.ceil(p5.height / this.scale)
-
-    this.cells = new Array(this.cols)
-    for (let i = 0; i < this.cells.length; i++) {
-      this.cells[i] = new Array(this.rows)
-    }
+    this.updated = true
     for (let i = 0; i < this.cells.length; i++) {
       for (let j = 0; j < this.cells[i].length; j++) {
-        this.cells[i][j] = new Cell(i, j, this.scale, myP5)
+        this.cells[i][j] = new Cell(
+          i,
+          j,
+          this.scale,
+          myP5,
+          this.cells[i][j]
+        )
       }
     }
   }
-
-  draw = p5 => {
-    if (!this.changed && this.state.found) {
-      const myP5 = p5
-      this.changed = true
-      for (let i = 0; i < this.cells.length; i++) {
-        for (let j = 0; j < this.cells[i].length; j++) {
-          this.cells[i][j] = new Cell(i, j, this.scale, myP5, this.cells[i][j])
-        }
-      }
-    }
-    p5.background(0)
-    if (p5.mouseIsPressed) {
-      const mouseX = p5.mouseX
-      const mouseY = p5.mouseY
-      for (let i = 0; i < this.cells.length; i++) {
-        for (let j = 0; j < this.cells[i].length; j++) {
-          this.cells[i][j].click(mouseX, mouseY)
-        }
-      }
-    }
+  p5.background(0)
+  if (p5.mouseIsPressed) {
+    const mouseX = p5.mouseX
+    const mouseY = p5.mouseY
     for (let i = 0; i < this.cells.length; i++) {
       for (let j = 0; j < this.cells[i].length; j++) {
-        this.cells[i][j].show()
+        this.cells[i][j].click(mouseX, mouseY)
       }
     }
-    // NOTE: Do not use setState in draw function or in functions that is executed in draw function... pls use normal variables or class properties for this purposes
   }
-  saveGrid = (event) => {
-    const map = this.cells.map(row => row.map(cell => cell.wall))
-    axios({
-      url: `${apiUrl}/grids/` + this.props.match.params.id,
-      method: 'PATCH',
-      data: { grid: { walls: map } },
-      headers: {
-        Authorization: `Bearer ${this.props.user.token}`
-      }
-    })
-      .then(res => {
-        console.log('yay :)')
-        // this.setState({ created: true, gridId: res.data.grid._id })
+  for (let i = 0; i < this.cells.length; i++) {
+    for (let j = 0; j < this.cells[i].length; j++) {
+      this.cells[i][j].show()
+    }
+  }
+  // NOTE: Do not use setState in draw function or in functions that is executed in draw function... pls use normal variables or class properties for this purposes
+}
+updateGrid = event => {
+  event.preventDefault()
+  const name = this.state.grid.name
+  const map = this.cells.map(row => row.map(cell => cell.wall))
+  axios({
+    url: `${apiUrl}/grids/` + this.props.match.params.id,
+    method: 'PATCH',
+    data: { grid: { walls: map, name: name } },
+    headers: {
+      Authorization: `Bearer ${this.props.user.token}`
+    }
+  })
+    .then(res => {
+      this.props.msgAlert({
+        heading: 'Successfully Updated Grid',
+        message: 'Your Grid was successfully saved to our Database',
+        variant: 'success'
       })
-      .catch(console.error)
-  }
-  render () {
+    })
+    .catch(res => {
+      this.props.msgAlert({
+        heading: 'Failed to Update Grid',
+        message: 'There may be a problem with your connection, please try refreshing the page',
+        variant: 'danger'
+      })
+    })
+}
+render () {
+  if (this.state.found) {
     return (
       <div>
-        <Sketch
-          setup={this.setup}
-          draw={this.draw}
-        />
-        {this.state.owned && <button onClick={this.saveGrid}>Save Changes</button>}
+        {this.state.owned && (
+          <GridForm
+            grid={this.state.grid}
+            handleChange={this.handleChange}
+            handleSubmit={this.updateGrid}
+          />
+        )}
+        <Sketch setup={this.setup} draw={this.draw} />
       </div>
     )
+  } else {
+    return <div>Loading....</div>
   }
+}
 }
