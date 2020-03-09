38ed50e367c78c58cd8f613ddedfe4b698fe08dd
joe10994 <joe10994@gmail.com>
2020-03-09 12:51:31 -0400

    Add boolean of whether a cell is a wall
    - If mouse is pressed, the cell that the mouse is over will
    change into a wall or back to a non-wall.
    - Added 1 second setTimeout to make editing easier

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -13,7 +13,9 @@ export default class Grid extends Component {
   cols
   cellTest
   rows
-  scale = 10
+  // TODO: Make breakpoints to have this be responsive on mobile:
+  // scale down and also make the canvas resize if the window is under a certain width
+  scale = 20
   componentDidMount () {
     this.setState({
     })
@@ -21,11 +23,11 @@ export default class Grid extends Component {
   setup = (p5, canvasParentRef) => {
     // create canvas
     const myP5 = p5
-    p5.createCanvas(400, 400).parent(
+    p5.createCanvas(600, 500).parent(
       canvasParentRef
     ) // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
-    this.cols = p5.width / this.scale
-    this.rows = p5.height / this.scale
+    this.cols = Math.ceil(p5.width / this.scale)
+    this.rows = Math.ceil(p5.height / this.scale)
     this.cells = new Array(this.cols)
     for (let i = 0; i < this.cells.length; i++) {
       this.cells[i] = new Array(this.rows)
@@ -39,6 +41,15 @@ export default class Grid extends Component {
 
   draw = p5 => {
     p5.background(0)
+    if (p5.mouseIsPressed) {
+      const mouseX = p5.mouseX
+      const mouseY = p5.mouseY
+      for (let i = 0; i < this.cells.length; i++) {
+        for (let j = 0; j < this.cells[i].length; j++) {
+          this.cells[i][j].click(mouseX, mouseY)
+        }
+      }
+    }
     for (let i = 0; i < this.cells.length; i++) {
       for (let j = 0; j < this.cells[i].length; j++) {
         this.cells[i][j].show()
@@ -47,16 +58,19 @@ export default class Grid extends Component {
     // NOTE: Do not use setState in draw function or in functions that is executed in draw function... pls use normal variables or class properties for this purposes
   }
 
-  mouseClicked = () => {
-
-  }
-
+  // mouseDragged = () => {
+  //   for (let i = 0; i < this.cells.length; i++) {
+  //     for (let j = 0; j < this.cells[i].length; j++) {
+  //       this.cells[i][j].click()
+  //     }
+  //   }
+  // }
   render () {
     return (
       <Sketch
         setup={this.setup}
         draw={this.draw}
-        mouseClicked={this.mouseClicked}
+        mouseDragged={this.mouseDragged}
       />
     )
   }
