8a1c99a416cad83fc4591c2058eca8c9ed9b2635
joe10994 <joe10994@gmail.com>
2020-03-09 22:28:36 -0400

    Refactor Home Component to functinal component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,58 +1,53 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, withRouter } from 'react-router-dom'
-// ---------------
+// --------------- libraries
 import axios from 'axios'
-// ---------------
+// ---------------url
 import apiUrl from '../../apiConfig'
-class Home extends Component {
-  constructor () {
-    super()
-    this.state = {
-      grids: []
-    }
-  }
+
+const Home = (props) => {
+  const [grids, setGrids] = useState([])
+  const { user } = props
   // Only do the axios req if user is signed in-------------
-  componentDidMount () {
-    if (this.props.user) {
+  useEffect(() => {
+    if (user) {
       axios({
         url: `${apiUrl}/grids`,
         method: 'get',
         headers: {
-          Authorization: `Bearer ${this.props.user.token}`
+          Authorization: `Bearer ${user.token}`
         }
       })
-        .then(res => this.setState({ grids: res.data.grids }))
+        .then(res => setGrids(res.data.grids))
         .catch(console.error)
     }
-  }
+  }, [])
 
   // If the user is signed in, map over the grids, otherwise just display a welcome component (html for now) ---------------
 
-  render () {
-    let gridsHtml
-    if (this.props.user) {
-      gridsHtml = this.state.grids.map(grid => (
-        <li key={grid.id}>
-          <Link to={`/grids/${grid._id}`}>{grid.name}</Link>
-        </li>
-      ))
-    } else {
-      // TODO: Change this to a component
-      gridsHtml = <div>Welcome to my Pathfinding Visualization App!!!</div>
-    }
-
-    return (
-      <div>
-        {this.props.user && <h4>All Grids</h4>}
-        {this.props.user && (
-          <Link to={'/new_grid'}>
-            <button >New Grid</button>
-          </Link>
-        )}
-        <ul>{gridsHtml}</ul>
-      </div>
-    )
+  let gridsHtml
+  if (user) {
+    gridsHtml = grids.map(grid => (
+      <li key={grid.id}>
+        <Link to={`/grids/${grid._id}`}>{grid.name}</Link>
+      </li>
+    ))
+  } else {
+    // TODO: Change this to a component
+    gridsHtml = <div>Welcome to my Pathfinding Visualization App!!!</div>
   }
+
+  return (
+    <div>
+      {user && <h4>All Grids</h4>}
+      {user && (
+        <Link to={'/new_grid'}>
+          <button >New Grid</button>
+        </Link>
+      )}
+      <ul>{gridsHtml}</ul>
+    </div>
+  )
 }
 
 export default withRouter(Home)
