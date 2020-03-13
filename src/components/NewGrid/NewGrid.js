52cf1104e2bc08b7051af2e8e7d50f15e19db090
joe10994 <joe10994@gmail.com>
2020-03-13 01:18:37 -0400

    Remove A* Algorithm logic from grid Components
    - Create 'runAStar.js' in lib

--- src/components/NewGrid/NewGrid.js
+++ src/components/NewGrid/NewGrid.js
@@ -13,8 +13,8 @@ import beginAStar from '../../lib/beginAStar'
 import findAllNeighbors from '../../lib/findAllNeighbors'
 import resetBoard from '../../lib/resetBoard'
 import checkForClicks from '../../lib/checkForClicks'
-import heuristic from '../../lib/heuristic'
-import removeFromArray from '../../lib/removeFromArray'
+
+import runAStar from '../../lib/runAStar'
 // -----------Libraries
 import axios from 'axios'
 // -----------API URL
@@ -50,6 +50,7 @@ export default class NewGrid extends Component {
   findAllNeighbors = findAllNeighbors.bind(this)
   resetBoard = resetBoard.bind(this)
   checkForClicks = checkForClicks.bind(this)
+  runAStar = runAStar.bind(this)
 
   // TODO: Make breakpoints to have this be responsive on mobile:
   // scale down and also make the canvas resize if the window is under a certain width
@@ -120,91 +121,8 @@ export default class NewGrid extends Component {
 
   draw = p5 => {
     p5.background(255)
-
+    this.runAStar(p5)
     this.checkForClicks(p5)
-    const { openSet, closedSet, end } = this
-    const { algorithm } = this.state
-    // while the algo is A* and there are still cells in openSet
-    if (algorithm === 'A*') {
-      if (openSet.length > 0) {
-        let winner = 0
-        // find the lowest score in the openSet (closest cell to end)
-        for (let i = 0; i < openSet.length; i++) {
-          if (openSet[i].f < openSet[winner].f) winner = i
-        }
-
-        this.current = openSet[winner]
-        const { current } = this
-        // if we found the solution...
-        if (current === end) {
-          this.setState({ algorithm: null })
-        } else {
-        // Remove from open set and add to closed, setting attr for visuals
-          removeFromArray(openSet, current)
-          current.open = false
-
-          closedSet.push(current)
-          current.closed = true
-          // find al the neighbors of the closest cell
-          const neighbors = current.neighbors
-          // loop through neighbors
-
-          neighbors.forEach((neighbor) => {
-          // if it is in the closed set, skip it, it's already been calculated
-            if (!closedSet.includes(neighbor) && !neighbor.wall) {
-              // if not, the tentative g score for that neighbor is current+1
-              const tempG = current.g + heuristic(current, neighbor, p5)
-              let newPath = false
-              // if it's in the open set, check if the new g is better
-              // if so , set it
-              if (openSet.includes(neighbor)) {
-                if (tempG < neighbor.g) {
-                  neighbor.g = tempG
-                  newPath = true
-                }
-
-              // if not in open set, just set it's g score without the check, and push into open set
-              } else {
-                newPath = true
-                neighbor.g = tempG
-                openSet.push(neighbor)
-                neighbor.open = true
-              }
-              // no matter what, find the new best heuristic of this neighbor
-              // set the f score
-              // set the previous for the path
-              if (newPath) {
-                neighbor.h = heuristic(neighbor, end, p5)
-                neighbor.f = neighbor.g + neighbor.h
-                neighbor.previous = current
-              }
-            }
-          })
-        }
-      } else {
-        this.setState({ algorithm: null })
-
-        // no solution
-      }
-      // on all loops, calculate the current best path
-      this.path = []
-      let temp = this.current
-      this.path.push(temp)
-      while (temp.previous) {
-        this.path.push(temp.previous)
-        temp = temp.previous
-      }
-    }
-    // reset the previous of all cells so that the path doesn't accumulate
-    for (let i = 0; i < this.cells.length; i++) {
-      for (let j = 0; j < this.cells[i].length; j++) {
-        this.cells[i][j].path = false
-      }
-    }
-    // set path
-    for (let i = 0; i < this.path.length; i++) {
-      this.path[i].path = true
-    }
 
     // continual loop to show all cells based on their state
 
